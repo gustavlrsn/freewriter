b40cc370d2be14ed37728b619356dae9fc6faa9f
Gustav Larsson <gustav.larsson@gmail.com>
2020-05-31 15:15:18 +0200

    add icon

--- ui/components/Icons.js
+++ ui/components/Icons.js
@@ -47,6 +47,20 @@ export const Loader = (props) => (
 );
 
 // Heroicons: badge-check
+export const BadgeAlt = (props) => (
+  <svg
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="2"
+    viewBox="0 0 24 24"
+    {...props}
+  >
+    <path d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"></path>
+  </svg>
+);
+
 export const Badge = (props) => (
   <svg fill="currentColor" viewBox="0 0 20 20" {...props}>
     <path
